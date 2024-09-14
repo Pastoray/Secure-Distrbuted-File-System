6f168dbf4439455d9d6428f8f22b096606d7a30a
Pastori <pastoriaraar69@gmail.com>
2024-09-14 03:07:13 +0200

    add encryption

--- client.cpp
+++ client.cpp
@@ -9,32 +9,42 @@
 #include <unordered_map>
 #include <functional>
 #include "utils.hpp"
+#include "transformer.hpp"
+#include "crypto.hpp"
 
-void handle_send(SOCKET clientSocket, std::string& req) {
-  int byteCount = send(clientSocket, req.c_str(), 200, 0);
-  if(byteCount > 0) {
+void handle_send(SOCKET clientSocket, std::string& req)
+{
+  std::string encryptedStr = Crypto::encrypt(req);
+  int byteCount = send(clientSocket, encryptedStr.c_str(), encryptedStr.size(), 0);
+  if(byteCount > 0)
     std::cout << "Sent message: \"" << req << "\"" << std::endl;
-  } else {
+  else
+  {
     std::cout << "Nothing was sent." << std::endl;
     WSACleanup();
-    return;
   }
 }
 
-void handle_recv(SOCKET acceptSocket) {
+void handle_recv(SOCKET acceptSocket)
+{
   char buffer[1024];
   memset(buffer, 0, sizeof(buffer));
-  int byteCount = recv(acceptSocket, buffer, 200, 0);
-  if(byteCount > 0) {
-    std::cout << "Received message: \"" << buffer << "\"" << std::endl;
-  } else {
+  int byteCount = recv(acceptSocket, buffer, sizeof(buffer), 0);
+  std::string decryptedRes;
+  if(byteCount > 0)
+  {
+    decryptedRes = Crypto::decrypt(buffer);
+    std::cout << "Received message: \"" << decryptedRes << "\"" << std::endl;
+  }
+  else
+  {
     std::cerr << "Didn't receive anything." << std::endl;
     WSACleanup();
   }
-  std::string str(buffer);
+  std::string str(decryptedRes);
   Res res = Transformer::Respons::deserialize(str);
   SetConsoleOutputCP(CP_UTF8); // to be able to cout emojis
-  print_res(res);
+  Transformer::Respons::print(res);
 }
 
 SOCKET create_socket()
@@ -45,38 +55,43 @@ SOCKET create_socket()
   WORD wVersionRequested = MAKEWORD(2, 2);
   int wsaerr = WSAStartup(wVersionRequested, &wsaData);
 
-  if(wsaerr != 0) {
-    std::cout << "Winsock dll not found." << std::endl;
-    return EXIT_FAILURE;
-  } else {
-    std::cout << "Winsock dll was found!" << std::endl;
+  if(wsaerr != 0)
+  {
+    std::cout << "Winsock dll not found" << std::endl;
+    err_exit("Winsock dll not found");
+  }
+  else
+  {
+    std::cout << "Winsock dll was found" << std::endl;
     std::cout << "Status: " << wsaData.szSystemStatus << std::endl;
   }
 
-  clientSocket = INVALID_SOCKET;
   clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-  if(clientSocket == INVALID_SOCKET) {
-    std::cout << "Error at socket()." << std::endl;
+  if(clientSocket == INVALID_SOCKET)
+  {
+    std::cout << "Error at socket()" << std::endl;
+    WSACleanup();
     return EXIT_FAILURE;
-  } else {
-    std::cout << "Socket() is OK!" << std::endl;
   }
+  else
+    std::cout << "Socket() is OK" << std::endl;
 
   sockaddr_in clientService;
   clientService.sin_family = AF_INET;
   InetPton(AF_INET, _T("127.0.0.1"), &clientService.sin_addr.s_addr);
   clientService.sin_port = htons(PORT);
 
-  if(connect(clientSocket, (SOCKADDR*)&clientService, sizeof(clientService)) == SOCKET_ERROR) {
-    std::cout << "Client connect() failed." << std::endl;
+  if(connect(clientSocket, (SOCKADDR*)&clientService, sizeof(clientService)) == SOCKET_ERROR)
+  {
+    std::cout << "Client connect() failed" << std::endl;
     WSACleanup();
-    return 0;
-  } else {
-    std::cout << "Client connect() is OK!" << std::endl;
+    return EXIT_FAILURE;
+  }
+  else
+  {
+    std::cout << "Client connect() is OK" << std::endl;
     std::cout << "Client can start sending and receiving data" << std::endl;    
   }
-
   return clientSocket;
 }
 
@@ -88,47 +103,67 @@ T create_req(Args&&... args)
 
 int main(int argc, char* argv[])
 {
+  if(argc == 2)
+  {
+    if(argv[1] == "?")
+    {
+      std::cout << "Usage: \n"
+        << "GET [FILE/DIR] <path>\n"
+        << "CREATE [FILE/DIR] <path> <name> <content (optional; only if file)>\n"
+        << "EDIT [FILE/DIR] <path> <name (optional)> <content (optional)> [OVERRIDE/APPEND]\n"
+        << "REMOVE [FILE/DIR] <path>" << std::endl;
+      return EXIT_SUCCESS;
+    }
+  }
   if(argc <= 2)
   {
-    std::cerr << "Wrong usage,\nCorrect usage: ./client <request> <args...>" << std::endl;
+    std::cerr << "Wrong usage,\nCorrect usage: ./client <request> <args...>\n"
+      << "Use \"./client ?\" for more details" << std::endl;
     return EXIT_FAILURE;
   }
-  
-  std::string request = argv[1], entity = argv[2], path = argv[3];
-  if(entity != "file" && entity != "dir")
+
+  /*
+    argv[1] = request type
+    argv[2] = entry type
+    argv[3] = path
+    argv[4] = name (create, edit)
+    argv[5] = content (create, edit)
+    argv[6] = edit type (edit)
+  */
+
+  if(argv[2] != "FILE" && argv[2] != "DIR")
   {
-    std::cerr << "Unrecognized entity: " << entity << std::endl;
+    std::cerr << "Invalid entity: " << argv[2] << std::endl;
     return EXIT_FAILURE;
   }
-  EntryType et = (entity == "file" ? EntryType::FILE : EntryType::DIR);
+  EntryType et = argv[2] == "FILE" ? EntryType::FILE : EntryType::DIR;
   std::unordered_map<std::string, std::function<Req()>> request_map = {
     {MessageType::GET, [&]() -> Request::Get {
-      auto req =  create_req<Request::Get>(path, et);
+      auto req =  create_req<Request::Get>(et, argv[3]);
       return req;
     }},
     {MessageType::CREATE, [&]() -> Request::Create {
-      auto req =  create_req<Request::Create>(path, et, argv[4], argv[5]);
+      auto req =  create_req<Request::Create>(et, argv[3], argv[4], argv[5]);
       return req;
     }},
     {MessageType::EDIT, [&]() -> Request::Edit {
-      auto req =  create_req<Request::Edit>(path, et, argv[4], argv[5], (UpdateType)std::stoi(argv[6]));
+      auto req =  create_req<Request::Edit>(et, argv[3], argv[4], argv[5], (EditType)std::stoi(argv[6]));
       return req;
     }},
     {MessageType::REMOVE, [&]() -> Request::Remove {
-      auto req =  create_req<Request::Remove>(path, et);
+      auto req =  create_req<Request::Remove>(et, argv[3]);
       return req;
     }}
   };
-  Req req;
-  if (request_map.find(request) != request_map.end())
-    req = request_map[request]();
+  Req request;
+  if (request_map.find(argv[1]) != request_map.end())
+    request = request_map[argv[1]]();
   else
     err_exit("Unknown request type");
   
   SOCKET socket = create_socket();
-  std::string str = Transformer::Reques::serialize(req);
-  std::string encryptedStr = Transformer::Reques::encrypt(str);
-  handle_send(socket, encryptedStr);
+  std::string str = Transformer::Reques::serialize(request);
+  handle_send(socket, str);
   handle_recv(socket);
   close_socket(socket);
   WSACleanup();
--- server.cpp
+++ server.cpp
@@ -6,12 +6,17 @@
 #include <thread>
 #include <windows.h>
 #include "utils.hpp"
+#include "transformer.hpp"
+#include "crypto.hpp"
 
-void handle_send(SOCKET clientSocket, std::string& response) {
-  int byteCount = send(clientSocket, response.c_str(), 200, 0);
-  if(byteCount > 0) {
-    std::cout << "Sent message: \"" << response << "\"" << std::endl;
-  } else {
+void handle_send(SOCKET clientSocket, std::string& res)
+{
+  std::string encryptedRes = Crypto::encrypt(res);
+  int byteCount = send(clientSocket, encryptedRes.c_str(), encryptedRes.size(), 0);
+  if(byteCount > 0)
+    std::cout << "Sent message: \"" << res << "\"" << std::endl;
+  else
+  {
     std::cout << "Nothing was sent." << std::endl;
     WSACleanup();
   }
@@ -21,31 +26,31 @@ void handle_receive(SOCKET acceptSocket)
 {
   char buffer[1024];
   memset(buffer, 0, sizeof(buffer));
-  int byteCount = recv(acceptSocket, buffer, 200, 0);
+  int byteCount = recv(acceptSocket, buffer, sizeof(buffer), 0);
+  std::string decryptedReq;
   if(byteCount > 0)
-    std::cout << "Received message: \"" << buffer << "\"" << std::endl;
+  {
+    decryptedReq = Crypto::decrypt(std::string(buffer, byteCount));
+    std::cout << "Received message: \"" << decryptedReq << "\"" << std::endl;
+  }
   else
   {
     std::cout << "Didn't receive anything." << std::endl;
     WSACleanup();
   }
-  std::string encryptedStr(buffer);
-  std::string str = Transformer::Reques::decrypt(encryptedStr);
-  auto req = Transformer::Reques::deserialize(str);
+  auto req = Transformer::Reques::deserialize(decryptedReq);
   auto res = process_request(req);
   std::string response = Transformer::Respons::serialize(res);
-  std::string encryptedResponse = Transformer::Respons::encrypt(response);
-  handle_send(acceptSocket, encryptedResponse);
+  handle_send(acceptSocket, response);
 }
 
 int main(int argc, char* argv[])
 {
-  if(argc != 3 || std::strcmp(argv[1], "--key") != 0 || std::strlen(argv[2]) != 64)
-  {
-    std::cerr << "Wrong usage,\nCorrect usage: ./server --key <32-bytes-key>" << std::endl;
-    return EXIT_FAILURE;
-  }
-  Transformer::set_key(argv[2]);
+  // if(argc != 3 || std::strcmp(argv[1], "--key") != 0 || std::strlen(argv[2]) != 64)
+  // {
+  //   std::cerr << "Wrong usage,\nCorrect usage: ./server --key <32-bytes-key>" << std::endl;
+  //   return EXIT_FAILURE;
+  // }
   
   SOCKET serverSocket, acceptSocket;
   const int PORT = 55555;
@@ -53,53 +58,59 @@ int main(int argc, char* argv[])
   WORD wVersionRequested = MAKEWORD(2, 2);
   int wsaerr = WSAStartup(wVersionRequested, &wsaData);
 
-  if (wsaerr != 0) {
-    std::cerr << "Winsock dll not found." << std::endl;
+  if (wsaerr != 0)
+  {
+    std::cerr << "Winsock dll not found" << std::endl;
     return EXIT_FAILURE;
-  } else {
-    std::cout << "Winsock dll was found!" << std::endl;
+  }
+  else
+  {
+    std::cout << "Winsock dll was found" << std::endl;
     std::cout << "Status: " << wsaData.szSystemStatus << std::endl;
   }
 
   serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-  if (serverSocket == INVALID_SOCKET) {
-    std::cerr << "Error at socket()." << std::endl;
+  if (serverSocket == INVALID_SOCKET)
+  {
+    std::cerr << "Error at socket()" << std::endl;
     WSACleanup();
     return EXIT_FAILURE;
-  } else {
-    std::cout << "Socket() is OK!" << std::endl;
   }
+  else
+    std::cout << "Socket() is OK" << std::endl;
 
   sockaddr_in service;
   service.sin_family = AF_INET;
   InetPton(AF_INET, _T("127.0.0.1"), &service.sin_addr.s_addr);
   service.sin_port = htons(PORT);
 
-  if (bind(serverSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
-    std::cerr << "bind() failed." << std::endl;
+  if (bind(serverSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR)
+  {
+    std::cerr << "bind() failed" << std::endl;
     std::cerr << WSAGetLastError() << std::endl;
     closesocket(serverSocket);
     WSACleanup();
     return EXIT_FAILURE;
-  } else {
-    std::cout << "bind() is OK!" << std::endl;
   }
+  else
+    std::cout << "bind() is OK!" << std::endl;
 
-  if (listen(serverSocket, 2) == SOCKET_ERROR) {
-    std::cerr << "listen() error listening on socket." << std::endl;
+  if (listen(serverSocket, 2) == SOCKET_ERROR)
+  {
+    std::cerr << "listen() error listening on socket" << std::endl;
     closesocket(serverSocket);
     WSACleanup();
     return EXIT_FAILURE;
-  } else {
-    std::cout << "listen() is OK!, waiting for connections.." << std::endl;
   }
+  else
+    std::cout << "listen() is OK!, waiting for connections.." << std::endl;
 
   while(true)
   {
     acceptSocket = accept(serverSocket, NULL, NULL);
-    if (acceptSocket == INVALID_SOCKET) {
-      std::cerr << "accept failed." << std::endl;
+    if (acceptSocket == INVALID_SOCKET)
+    {
+      std::cerr << "accept failed" << std::endl;
       std::cerr << WSAGetLastError() << std::endl;
       closesocket(serverSocket);
       WSACleanup();
